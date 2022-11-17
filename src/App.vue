d4db3604348e900a05d27bfcacffd2401ddeca19
doni255 <wijayadoni37@gmail.com>
2022-11-17 21:23:16 +0700

    UGD_FINAL

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
   </v-app>
 </template>
 
+<style>
+@import "../node_modules/@syncfusion/ej2-base/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-inputs/styles/material.css";
+@import "../node_modules/@syncfusion/ej2-vue-dropdowns/styles/material.css";
+</style>
+
 <script>
 export default {
   name: 'App',
--- src/router.js
+++ src/router.js
@@ -21,7 +21,14 @@ const router = new VueRouter ({
                     name: 'MerchandisePage',
                     meta: {title: 'Vuefire 2022'},
                     component: importView('MerchandisePage'),
+                },
+                {
+                    path: '/album',
+                    name: 'AlbumPage',
+                    meta: {title: 'Vuefire 2022'},
+                    component: importView('AlbumPage'),
                 }
+                
             ]
         }
     ]
